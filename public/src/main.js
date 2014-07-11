9c696f86cd34b456838c6dff03e0d1ee864dcad1
Jordan Curzon <curzonj@gmail.com>
2014-07-10 20:01:37 -0700

    wip

--- public/src/main.js
+++ public/src/main.js
@@ -7,27 +7,34 @@ window.onload = function() {
     });
 
     function preload() {
-        game.load.image('ship', 'assets/images/shmup-ship.png');
-        game.load.image('starfield', 'assets/images/starfield.jpg');
+        game.load.image('ship', '/images/shmup-ship.png');
+        game.load.image('starfield', '/images/starfield.jpg');
     }
 
     var s;
     var player;
+    var cursors;
 
     function create() {
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+
         s = game.add.tileSprite(0, 0, 800, 600, 'starfield');
 
         player = game.add.sprite(game.world.centerX, game.world.centerY, 'ship');
         player.anchor.setTo(0.5, 0.5);
+
+        game.physics.enable(player, Phaser.Physics.ARCADE);
+
+        cursors = game.input.keyboard.createCursorKeys();
     }
 
+
     function update() {
-        if (cursors.left.isDown) {
+        /* if (cursors.left.isDown) {
             player.body.velocity.x = -200;
         } else if (cursors.right.isDown) {
             player.body.velocity.x = 200;
-        }
-
+        } */
     }
 
 };
